92c6fc8f0f92e9238556821bb3b7ef20daaaf51f
John Maturan <jmaturan@deloitte.com>
2022-01-07 09:25:55 -0500

    Home Page Slider Banner

--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -2,14 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MoviesService {
-  constructor(private http: HttpClient) {
-
-  }
+  constructor(private http: HttpClient) {}
 
   getMovies() {
-    return this.http.get('https://api.themoviedb.org/3/movie/upcoming?api_key=be9851fb6b6fa84b4634909d0e891b85')
+    return this.http.get(
+      'https://api.themoviedb.org/3/movie/upcoming?api_key=be9851fb6b6fa84b4634909d0e891b85'
+    );
   }
 }
